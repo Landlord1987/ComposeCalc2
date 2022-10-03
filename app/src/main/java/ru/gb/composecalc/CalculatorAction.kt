ec954c1a0088fbde843f6da0a6737e9e0d518212
DenisKrinitsyn <skylinekrin@gmail.com>
2022-10-03 20:52:13 +0500

    ComposeCalc

---
+++ app/src/main/java/ru/gb/composecalc/CalculatorAction.kt
@@ -0,0 +1,10 @@
+package ru.gb.composecalc
+
+sealed class CalculatorAction {
+    data class Number(val number: Int) : CalculatorAction()
+    object Clear:CalculatorAction()
+    object Delete:CalculatorAction()
+    object Decimal:CalculatorAction()
+    object Calculate:CalculatorAction()
+    data class Operation(val operation: CalculatorOperation) : CalculatorAction()
+}
---
+++ app/src/main/java/ru/gb/composecalc/MainActivity.kt
@@ -0,0 +1,44 @@
+package ru.gb.composecalc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ru.gb.composecalc.ui.theme.ComposeCalcTheme
+import ru.gb.composecalc.ui.theme.MediumGray
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeCalcTheme {
+               val viewModel = viewModel<CalculatorViewModel>()
+               val state = viewModel.state
+               val buttonSpacing = 8.dp
+                Calculator(state = state, onAction = viewModel::onAction,
+                buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+
+
